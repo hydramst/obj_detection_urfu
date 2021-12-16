edb7c52366cce8eca71b5c7bacb1ec4c2d0ca2cb
Nivt17 <65940534+Nivt17@users.noreply.github.com>
2021-12-16 19:59:54 +0500

    Update image_detection_api.py

--- image_detection_api.py
+++ image_detection_api.py
@@ -2,6 +2,17 @@ import image_detection_model as fl
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+import requests
+import io
+import scipy.misc
+import numpy as np
+import tensorflow as tf
+import tensorflow_hub as hub
+import pickle
+
+MODELS_path = 'https://tfhub.dev/tensorflow/centernet/resnet101v1_fpn_512x512/1'
+hub_model = hub.load(MODELS_path) 
+
 class Item(BaseModel):
     link: str
 
@@ -13,5 +24,5 @@ async def root():
 
 @app.post("/predict/")
 def predict(item: Item):
-    return fl.detect(item.link)
+    return fl.detect(item.link, hub_model)
 
