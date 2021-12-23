6a10ba255d30ca91252f8a190b9dc09c243b111f
Nivt17 <65940534+Nivt17@users.noreply.github.com>
2021-12-23 17:49:20 +0500

    Update image_detection_model.py

--- image_detection_model.py
+++ image_detection_model.py
@@ -1,4 +1,13 @@
 import requests
+import scipy.misc
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+import clases as cl
+
+from six import BytesIO
+from PIL import Image, ImageDraw, ImageFont #lib - Pillow
+from six.moves.urllib.request import urlopen
 
 def load_image_into_numpy_array(path):
   image = None
@@ -17,5 +26,13 @@ def load_image_into_numpy_array(path):
 
 def detect(image, hub_model):
     image_np = load_image_into_numpy_array(image)
-    result = hub_model(image_np)
+    ans = hub_model(image_np)
+    detection_class_entities = map(int, ans["detection_classes"].numpy().tolist()[0])
+    detection_scores = ans["detection_scores"].numpy().tolist()[0]
+    res = dict(zip(detection_class_entities, detection_scores))
+
+    df_1 = pd.DataFrame({'classes_id': list(res.keys()), 'scores': list(res.values())})
+    df_2 = pd.DataFrame({'classes_id': list(cl.classes.keys()), 'name_class': list(cl.classes.values())})
+    fin_df = df_1.merge(df_2, left_on='classes_id', right_on='classes_id')[['name_class', 'scores']]
+    result = dict(zip(fin_df['name_class'], fin_df['scores']))
     return result
